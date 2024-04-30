2bc73dd2b1117636da79b5803d7e2062b7c8cb72
smit prajapati <smitprajapati002@gmail.com>
2024-04-30 17:37:52 -0600

    Some basic programs commited

--- basic.py
+++ basic.py
@@ -1,1 +1,34 @@
-print("hello world")
+# print("hello world")
+
+
+# Assigning value in python, There are different ways to assign them
+
+# a = 23 # this is fist and this is integer value
+# b = "smit" # this is string 
+
+# x = y = z = 22 # You can also assing vale like tis
+
+# You can cast variable with some syntax such as 
+
+x = int(1)
+y = str("smit")
+z = float(223.2)
+# And you can also convert the int, float, and string  with upper syntax
+
+
+# You can write multiline string
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+# In triple quote you can wtite multiline comment
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a) # as well as you can write in three single quote 
+
+
