45210abf2b402a14e2a4f6da23c495ca8e535c82
smit prajapati <smitprajapati002@gmail.com>
2024-05-07 16:04:24 -0600

    Iterator concept is over

--- oops.py
+++ oops.py
@@ -3,13 +3,13 @@
 # Parent class is the class being inherited from, also caled base class.
 # Child class is the class that inherits from another class, also called derived class.
 
-class Teacher:
-    def __init__(self, fName, lName):
-        self.fName = fName
-        self.lName = lName
+# class Teacher:
+#     def __init__(self, fName, lName):
+#         self.fName = fName
+#         self.lName = lName
 
-    def printName(self):
-        print(self.fName, self.lName)
+#     def printName(self):
+#         print(self.fName, self.lName)
     
 # class Student(Teacher):
 #     pass # We use pass when we don't want to pass any method or other values in class
@@ -17,12 +17,28 @@ class Teacher:
 # x.printName()
 # Python has super() keyword that will make the child(derived class) class inherit all the methods and properties from its parent(base class)
 
-class Student(Teacher):
-    def __init__(self, fName, lName, year):
-        super().__init__(fName, lName)
-        self.graduatinYear = year
+# class Student(Teacher):
+#     def __init__(self, fName, lName, year):
+#         super().__init__(fName, lName)
+#         self.graduatinYear = year
 
-x = Student("Smit", "Prajapati", 2023)
-print(x.graduatinYear)
+# x = Student("Smit", "Prajapati", 2023)
+# print(x.graduatinYear)
 
-print(x)
+# print(x)
+
+
+# Iterators in Python
+
+# A Iterator is object that content countable number of values.
+# An iterator is an object that can be iterated upon, meaning that you can traverse through all the values.
+# Technically, in Python, an iterator is an object which implements the iterator protocol, which consist of the methods __iter__() and __next__().
+
+# myTup = ("mv", "tv", "av")
+myTup = ("smit")
+
+myIt = iter(myTup)
+print(next(myIt))
+print(next(myIt))
+print(next(myIt))
+print(next(myIt))
