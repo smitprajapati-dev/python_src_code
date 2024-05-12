918a849de21a06a660f1f2d74c75ee8b112743de
smit prajapati <smitprajapati002@gmail.com>
2024-05-12 16:19:50 -0600

    json files in python concept is over

---
+++ currency_converter.py
@@ -0,0 +1,4 @@
+with open('currencyData.txt') as f:
+    a = f.readline()
+
+print(a)
---
+++ json.py
@@ -0,0 +1,36 @@
+import json
+
+# a Python object (dict):
+# x = {
+#   "name": "John",
+#   "age": 30,
+#   "city": "New York"
+# }
+
+# # convert into JSON:
+# y = json.dumps(x)
+
+# # the result is a JSON string:
+# print(y)
+
+# You can convert python object type, into json
+ 
+ # dict
+ # list
+ # tupl
+ # string
+ # int 
+ # float
+ # True
+ # False
+ # none
+
+print(json.dumps({"name": "John", "age": 30}))
+print(json.dumps(["apple", "bananas"]))
+print(json.dumps(("apple", "bananas")))
+print(json.dumps("hello"))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
